0aa91c1873bae17d52774c3d0ceedf68a91bd806
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-05-21 19:19:32 +0700

    Initial commit

---
+++ lib/HashData/CPAN/AuthorName/ByPAUSEID.pm
@@ -0,0 +1,25 @@
+package HashData::CPAN::AuthorName::ByPAUSEID;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use Role::Tiny::With;
+with 'HashDataRole::Source::LinesInDATA';
+with 'Role::TinyCommons::Collection::FindItem::Iterator';         # add find_item() (has_item already added above)
+#with 'Role::TinyCommons::Collection::PickItems::RandomSeekLines'; # add pick_items() that uses binary search
+
+# STATS
+
+1;
+# ABSTRACT: CPAN authors by PAUSE ID
+
+=head1 DESCRIPTION
+
+For testing.
+
+=cut
+
+__DATA__
+# CODE: require App::lcpan::Call; my $res = App::lcpan::Call::call_lcpan_script(argv=>["authors", "-l"]); die unless $res->[0]==200; for my $rec (@{$res->[2]}) { print "$rec->{id}:$rec->{name}\n" }
---
+++ lib/HashDataBundle/CPAN.pm
@@ -0,0 +1,13 @@
+package HashDataBundle::CPAN;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: HashData::* modules related to CPAN
+
+=head1 SEE ALSO
+
+L<HashData>
